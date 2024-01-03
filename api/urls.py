e8415e9d7338f1d8ab6ad343952ba5dd9c5a2430
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-03 16:12:07 -0300

    :sparkles: feat: Implementando swagger na API DRF

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
-
-from rest_framework.routers import DefaultRouter
-from .views import PropostaViewSet
-
-router = DefaultRouter()
-router.register(r'propostas', PropostaViewSet, basename='proposta')
-urlpatterns = router.urls
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('', views.acess_proposta, name='acess-emprestimo'),
+    
+]
